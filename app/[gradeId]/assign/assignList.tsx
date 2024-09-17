db245207545dc449f76b6b6dc3e7cda885912b3d
Mohammad <mirrkamali@gmail.com>
2024-09-17 15:15:20 +0330

    design assign page

---
+++ app/[gradeId]/assign/assignList.tsx
@@ -0,0 +1,109 @@
+'use client';
+
+import React, { useMemo } from 'react';
+import { useFieldArray, useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { AssignFormType, ClassroomType, CourseType, TeacherType } from 'app/types/common.type';
+import FormSelect from 'app/components/formSelect';
+import fa from 'app/lib/fa.json';
+import Button from 'app/components/button';
+import { UpdateAssignmentAction } from 'app/lib/actions';
+import { tagRevalidate } from 'app/lib/server.util';
+
+const rawRow = {
+  class: null,
+  course: null,
+  teacher: null,
+};
+
+const AssignList: React.FC<{
+  classes: ClassroomType[];
+  teachers: TeacherType[];
+  courses: CourseType[];
+  assign: AssignFormType;
+  tag: string;
+}> = ({ classes, teachers, courses, assign, tag }) => {
+  const { gradeId } = useParams();
+  const rules = { required: true };
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+    watch,
+  } = useForm<AssignFormType>({ defaultValues: assign });
+
+  const hasChange = JSON.stringify(assign.assignments) !== JSON.stringify(watch('assignments'));
+  const { fields, append, remove } = useFieldArray({ control, name: 'assignments' });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: (e: AssignFormType) => UpdateAssignmentAction(e, gradeId.toString()),
+    onSuccess: (ok) => ok && tagRevalidate(tag),
+  });
+
+  const classOptions = useMemo(
+    () => classes?.map((k) => ({ value: k.id, label: k.title })) || [],
+    [classes]
+  );
+  const teacherOptions = useMemo(
+    () => teachers?.map((k) => ({ value: k.id, label: k.name })) || [],
+    [teachers]
+  );
+  const courseOptions = useMemo(
+    () => courses?.map((k) => ({ value: k.id, label: k.name })) || [],
+    [courses]
+  );
+
+  return (
+    <div>
+      <form className="flex flex-col gap-6 pb-20" onSubmit={handleSubmit((e) => mutate(e))}>
+        {fields.map((field, index) => (
+          <div className="flex gap-2 items-center" key={field.id}>
+            <FormSelect
+              {...{ errors, control, rules }}
+              name={`assignments.${index}.class`}
+              options={classOptions}
+              className="flex-1"
+              placeholder={fa.assign.chooseClass}
+            />
+            <FormSelect
+              {...{ errors, control, rules }}
+              name={`assignments.${index}.course`}
+              options={courseOptions}
+              className="flex-1"
+              placeholder={fa.assign.chooseCourse}
+            />
+            <FormSelect
+              {...{ errors, control, rules }}
+              name={`assignments.${index}.teacher`}
+              options={teacherOptions}
+              className="flex-1"
+              placeholder={fa.assign.chooseTeacher}
+            />
+            {fields.length > 1 && (
+              <i
+                className="icon-trash text-24 text-red70 cursor-pointer"
+                onClick={() => remove(index)}
+              />
+            )}
+          </div>
+        ))}
+        <Button type="button" className="btn btn-sm btn-accent w-44" onClick={() => append(rawRow)}>
+          <i className="icon-plus text-24" />
+          {fa.assign.addNewRow}
+        </Button>
+
+        {hasChange && (
+          <div className="fixed bottom-0 w-full bg-white50 p-3 left-0 text-left">
+            <Button isLoading={isPending} className="btn btn-primary w-40" type="submit">
+              {fa.assign.submit}
+            </Button>
+          </div>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default AssignList;
---
+++ app/[gradeId]/assign/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Metadata } from 'next';
+import fa from 'app/lib/fa.json';
+import {
+  AssignType,
+  ClassroomType,
+  CourseType,
+  PageType,
+  TeacherType,
+} from 'app/types/common.type';
+import { assignTag, classroomTag, courseTag, fetchData, teacherTag } from 'app/lib/server.util';
+import { GetAssignUrl, ShowClassUrl, ShowCourseUrl, ShowTeacherUrl } from 'app/lib/urls';
+import AssignList from './assignList';
+import { normalizeAssignData } from 'app/utils/common.util';
+
+export const metadata: Metadata = { title: fa.sidebar.teacher };
+
+const AssignPage: React.FC<PageType> = async ({ params }) => {
+  const [classes, teachers, courses, assignData] = await Promise.all([
+    fetchData<ClassroomType[]>(ShowClassUrl(params?.gradeId), classroomTag()),
+    fetchData<TeacherType[]>(ShowTeacherUrl(params?.gradeId), teacherTag()),
+    fetchData<CourseType[]>(ShowCourseUrl(params.gradeId), courseTag()),
+    fetchData<AssignType[]>(GetAssignUrl(params.gradeId), assignTag()),
+  ]);
+
+  return (
+    <div className="">
+      <h1 className="font-bold text-berry100 text-24 mb-10">{fa.sidebar.assign}</h1>
+      <AssignList
+        tag={assignTag()}
+        {...{ classes, teachers, courses }}
+        assign={normalizeAssignData(assignData, courses, teachers, classes)}
+      />
+    </div>
+  );
+};
+
+export default AssignPage;

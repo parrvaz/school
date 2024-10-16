3acb40d2380b7f05a8008f8d8ea4611e6bb8017c
Mohammad <mirrkamali@gmail.com>
2024-10-16 09:47:52 +0330

    fix a bug

--- app/(errors)/returnButton.tsx
+++ app/(errors)/returnButton.tsx
@@ -1,24 +1,17 @@
 'use client';
 
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Button from 'app/components/button';
 import fa from 'app/lib/fa.json';
 
 const ReturnButton: React.FC = () => {
   const router = useRouter();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
 
   return (
-    mounted && (
-      <Button onClick={() => router.back()} className="btn mt-4 btn-ghost">
-        {fa.global.return}
-      </Button>
-    )
+    <Button onClick={() => router.back()} className="btn mt-4 btn-ghost">
+      {fa.global.return}
+    </Button>
   );
 };
 

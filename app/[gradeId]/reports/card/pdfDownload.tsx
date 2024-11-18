e516bd9636e640852c967ed2cf3b4b0589a2245d
Mohammad <mirrkamali@gmail.com>
2024-11-18 18:11:44 +0330

    fix styles

--- app/[gradeId]/reports/card/pdfDownload.tsx
+++ app/[gradeId]/reports/card/pdfDownload.tsx
@@ -1,32 +1,24 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { PDFDocument } from 'pdf-lib';
-import { toPng } from 'html-to-image';
-import { saveAs } from 'file-saver';
+import React, { useState } from 'react';
 import Button from 'app/components/button';
 import fa from 'app/lib/fa.json';
 import { GroupDateType, ReportCardType } from 'app/types/common.type';
-import { faNumber } from 'app/utils/common.util';
-import Modal from 'app/components/modal';
 
 const PdfDownload: React.FC<{
   data: ReportCardType[] | undefined;
   date: GroupDateType;
   gradeId: number;
 }> = ({ data, date, gradeId }) => {
-  const [openModal, setOpenModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const name = `${fa.reports.card.card}-${date.startDate}-${date.endDate}`;
 
   const downloadPDF = async (): Promise<void> => {
     setLoading(true);
-    console.log(1);
     try {
       const response = await fetch('/api/generate-pdf', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ data, gradeId }),
       });
-      console.log(2, response);
 
       if (!response.ok) {
         throw new Error('Failed to generate PDF');
@@ -44,93 +36,13 @@ const PdfDownload: React.FC<{
   };
 
   return (
-    <>
-      <Button
-        onClick={downloadPDF}
-        isLoading={loading}
-        className=" btn btn-primary w-32 ml-4 btn-outline"
-      >
-        {fa.reports.card.downloadPdf}
-      </Button>
-
-      <Modal
-        className="min-w-[54rem] p-0 text-center flex flex-col items-center relative bg-berry10"
-        open={openModal}
-        setOpen={setOpenModal}
-        id="download-card"
-      >
-        <div className="mt-6 font-bold text-18 mb-4">{fa.global.download}</div>
-        <div className="">
-          {data?.map((item, index) => (
-            <div
-              key={item.name}
-              ref={(el) => {
-                elementRefs.current[index] = el;
-              }}
-              className="w-[794px] h-[1123px] p-7 bg-white"
-            >
-              <div className="flex gap-3 mb-2">
-                <div className="font-semibold leading-5 text-13">
-                  <div className="">{i18n.info1}</div>
-                  <div className="">{i18n.info2}</div>
-                  <div className="">{i18n.info3}</div>
-                  <div className="">{i18n.info4}</div>
-                  <div className="">{fa.global[`grade${gradeId}th`]}</div>
-                </div>
-                <div className="flex-1 border border-black60 text-12 text-start px-1 bg-black30 font-semibold">
-                  <div className="">{i18n.info5}</div>
-                  <div className="">{i18n.info6}</div>
-                  <div className="">{i18n.info7}</div>
-                  <div className="">{i18n.info8}</div>
-                </div>
-                <div className="flex-1 border border-black60 text-12 text-start px-1 bg-black30 font-semibold">
-                  <div className="">
-                    {i18n.info9} {item.firstName ? `: ${item?.firstName}` : ''}
-                  </div>
-                  <div className="">
-                    {i18n.info10} {item.lastName ? `: ${item.lastName}` : ''}
-                  </div>
-                  <div className="">{i18n.info11}</div>
-                  <div className="">{i18n.info12}</div>
-                </div>
-                <div className="w-20 h-24 border border-black60" />
-              </div>
-              <div className="text-center border border-black50 rounded-md overflow-hidden">
-                <table className="w-full rtl">
-                  <thead className="border-b border-b-black50 te">
-                    <tr>
-                      <th className="p-2">{fa.global.courseName}</th>
-                      <th className="p-2 border-x border-x-black50">{fa.global.factor}</th>
-                      <th className="p-2">{fa.global.score}</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {item?.scores?.map((item, index) => (
-                      <tr key={index} className={`${index % 2 ? 'bg-[#eee]' : 'bg-white'}`}>
-                        <td className="p-1">{item.course}</td>
-                        <td className="p-1 border-x border-x-black50">{faNumber(item.factor)}</td>
-                        <td className="p-1">{faNumber(item.score)}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-
-              <div className="flex items-center w-fit font-bold border border-black50 rounded-md mt-5">
-                <div className="border-l border-l-black50 py-2 px-5">{fa.global.average}</div>
-                <div className="px-5 text-20">{faNumber(item?.average)}</div>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="sticky bottom-0 bg-berry10 w-full text-end p-3">
-          <Button onClick={downloadPDF} className="btn w-56 btn-primary">
-            {fa.reports.card.downloadPdf}
-          </Button>
-        </div>
-      </Modal>
-    </>
+    <Button
+      onClick={downloadPDF}
+      isLoading={loading}
+      className=" btn btn-primary w-44 ml-4 btn-outline"
+    >
+      {fa.reports.card.downloadPdf}
+    </Button>
   );
 };
 
--- app/api/generate-pdf/route.ts
+++ app/api/generate-pdf/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
 import fa from 'app/lib/fa.json';
+import { faNumber } from 'app/utils/common.util';
 
 // POST handler
 export async function POST(req: Request): Promise<NextResponse<unknown>> {
@@ -14,81 +15,153 @@ export async function POST(req: Request): Promise<NextResponse<unknown>> {
 
     // Set HTML content dynamically
     await page.setContent(`
-      <!DOCTYPE html>
-      <html lang="fa" dir="rtl">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <style>
-           @font-face {
-            font-family: 'IRANSans_Medium';
-            src: url('./../../../public/fonts/IRANSansX-Medium.ttf');
-          }
-         body {
-            font-family: 'IRANSans_Medium', Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            background: #f8fafc;
-          }
-          .page {
-            width: 794px;
-            height: 1123px;
-            padding: 28px;
-            background: white;
-            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
-          }
-          .flex { display: flex; gap: 12px; }
-          .gap-3 { gap: 12px; }
-          .border { border: 1px solid #475569; }
-          .font-bold { font-weight: bold; }
-          .font-semibold { font-weight: 600; }
-          .text-13 { font-size: 0.8125rem; line-height: 1.125rem; }
-          .text-12 { font-size: 0.75rem; line-height: 1.5rem; }
-          .text-20 { font-size: 1.25rem; }
-          .rtl { direction: rtl; }
-          .text-center { text-align: center; }
-          .p-7 { padding: 28px; }
-          .mb-2 { margin-bottom: 8px; }
-          .w-[794px] { width: 794px; }
-          .h-[1123px] { height: 1123px; }
-          .bg-black30 { background: #cbd5e1; }
-          .bg-white { background: white; }
-          .overflow-hidden { overflow: hidden; }
-          .table {
-            width: 100%;
-            border-collapse: collapse;
-          }
-          .table th, .table td {
-            border: 1px solid #64748b;
-            padding: 8px;
-            text-align: center;
-          }
-          .table th {
-            background-color: #cbd5e1;
-          }
-          .rounded-md { border-radius: 0.375rem; }
-        </style>
-      </head>
-      <body>
-        <div>
-          ${data
-            .map(
-              (item) => `
-              <div class="page">
-            111
+    <!DOCTYPE html>
+    <html lang="fa" dir="rtl">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Vazirmatn:wght@400;700&display=swap');
+        body {
+          font-family: 'Vazirmatn', sans-serif;
+          margin: 0;
+          padding: 0;
+          background: #f8fafc;
+        }
+        .page {
+          width: 794px;
+          height: 1123px;
+          padding: 28px;
+          background: white;
+          box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+          page-break-after: always;
+        }
+        table {
+          width: 100%;
+          direction: rtl;
+          border-collapse: collapse;
+        }
+        th, td {
+          padding: 0.25rem;
+          border: 1px solid #64748b;
+          text-align: center;
+          font-size: 0.875rem;
+        }
+        th {
+          background: #f1f5f9;
+          font-size: 1rem;
+        }
+        tr:nth-child(odd) {
+          background: #eee;
+        }
+        tr:nth-child(even) {
+          background: #fff;
+        }
+      </style>
+    </head>
+    <body>
+      <div>
+        ${data
+          .map(
+            (item) => `
+            <div class="page">
+              <div style="background-color: #FFF; padding: 1.75rem; width: 794px; height: 1123px;">
+                <div style="display: flex; gap: 0.75rem; margin-bottom: 0.5rem;">
+                  <div style="font-weight: 600; text-align: center; line-height: 1.25rem; font-size: 0.8125rem;">
+                    <div>${i18n.info1}</div>
+                    <div>${i18n.info2}</div>
+                    <div>${i18n.info3}</div>
+                    <div>${i18n.info4}</div>
+                    <div>${fa.global[`grade${gradeId}th`]}</div>
+                  </div>
+                  <div
+                    style="
+                      flex: 1;
+                      border: 1px solid #475569;
+                      font-size: 0.75rem;
+                      text-align: start;
+                      padding: 0.25rem 0.5rem;
+                      background-color: #eee;
+                      font-weight: 600;
+                      display: flex;
+                      flex-direction: column;
+                      gap: 6px;
+                    "
+                  >
+                    <div>${i18n.info5}</div>
+                    <div>${i18n.info6}</div>
+                    <div>${i18n.info7}</div>
+                    <div>${i18n.info8}</div>
+                  </div>
+                  <div
+                    style="
+                      flex: 1;
+                      border: 1px solid #475569;
+                      font-size: 0.75rem;
+                      text-align: start;
+                      padding: 0.25rem 0.5rem;
+                      background-color: #eee;
+                      font-weight: 600;
+                      display: flex;
+                      flex-direction: column;
+                      gap: 6px;
+                    "
+                  >
+                    <div>${i18n.info9} ${item.firstName ? `: ${item?.firstName}` : ''}</div>
+                    <div>${i18n.info10} ${item.lastName ? `: ${item.lastName}` : ''}</div>
+                    <div>${i18n.info11}</div>
+                    <div>${i18n.info12}</div>
+                  </div>
+                  <div style="width: 5rem; height: 6rem; border: 1px solid #475569;"></div>
+                </div>
+                <div style="text-align: center; border: 1px solid #64748b; border-radius: 0.375rem; overflow: hidden;">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>${fa.global.courseName}</th>
+                        <th>${fa.global.factor}</th>
+                        <th>${fa.global.score}</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      ${item?.scores
+                        ?.map(
+                          (score) => `
+                          <tr>
+                            <td>${score.course}</td>
+                            <td>${faNumber(score.factor)}</td>
+                            <td>${faNumber(score.score)}</td>
+                          </tr>
+                        `
+                        )
+                        .join('')}
+                    </tbody>
+                  </table>
+                </div>
+
+                <div style="display: flex; align-items: center; width: fit-content; font-weight: bold; border: 1px solid #475569; border-radius: 0.375rem; margin-top: 1.25rem;">
+                    <div style="border-left: 1px solid #475569; padding-top: 0.5rem; padding-bottom: 0.5rem; padding-left: 1.25rem; padding-right: 1.25rem;">
+                        ${fa.global.average}
+                    </div>
+                    <div style="padding-left: 1.25rem; padding-right: 1.25rem; font-size: 1.25rem;">
+                        ${faNumber(item?.average)}
+                    </div>
+                </div>
               </div>
-            `
-            )
-            .join('')}
-        </div>
-      </body>
-      </html>
+            </div>
+          `
+          )
+          .join('')}
+      </div>
+    </body>
+    </html>
     `);
 
     // Generate the PDF
     const pdfBuffer = await page.pdf({
       format: 'A4',
       printBackground: true,
+      margin: { top: 0, right: 0, bottom: 0, left: 0 },
     });
 
     await browser.close();

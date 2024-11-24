1798b7c1d99a86f235c1ebc9bb1fe80cc42277c6
Mohammad <mirrkamali@gmail.com>
2024-11-24 07:16:06 +0330

    remove extra codes

--- app/[gradeId]/homework-list/page.tsx
+++ app/[gradeId]/homework-list/page.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import { Metadata } from 'next';
 import fa from 'app/lib/fa.json';
-import StudentsTable from './studentsTable';
-import {
-  absentsTag,
-  classroomTag,
-  courseTag,
-  examTag,
-  fetchData,
-  plansTag,
-  studentTag,
-} from 'app/lib/server.util';
-import { ShowClassUrl, ShowCourseUrl, ShowStudentUrl } from 'app/lib/urls';
-import { ClassroomType, CourseType, PageType, StudentType } from 'app/types/common.type';
+import { classroomTag, courseTag, fetchData } from 'app/lib/server.util';
+import { ShowClassUrl, ShowCourseUrl } from 'app/lib/urls';
+import { ClassroomType, CourseType, PageType } from 'app/types/common.type';
 import HomeworkList from '.';
 import CreateHomework from './createHomework';
 
@@ -20,11 +11,9 @@ export const metadata: Metadata = { title: fa.sidebar.homeworkList };
 
 const HomeworkListPage: React.FC<PageType> = async ({ params, searchParams }) => {
   const activeTab = searchParams?.tab || '';
-  const tags = [await studentTag(), await absentsTag(), await examTag(), await plansTag()];
   const [courses, classes] = await Promise.all([
     fetchData<CourseType[]>(ShowCourseUrl(params?.gradeId), await courseTag()),
     fetchData<ClassroomType[]>(ShowClassUrl(params?.gradeId), await classroomTag()),
-    // fetchData<StudentType[]>(ShowStudentUrl(params.gradeId), tags[0]),
   ]);
 
   const components = {
@@ -42,7 +31,6 @@ const HomeworkListPage: React.FC<PageType> = async ({ params, searchParams }) =>
       </h1>
 
       {components[activeTab] || <HomeworkList />}
-      {/* <StudentsTable data={data} classes={classes} tags={tags} /> */}
     </div>
   );
 };

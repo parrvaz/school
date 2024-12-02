a327eb9ea541a160f5356b69a0d27bd2ba38881f
Mohammad <mirrkamali@gmail.com>
2024-12-02 06:36:23 +0330

    fix a bug

--- app/[gradeId]/layout.tsx
+++ app/[gradeId]/layout.tsx
@@ -23,7 +23,7 @@ const menu = [
   { title: 'assign', icon: 'icon-book' },
   { title: 'createExam', icon: 'icon-path' },
   { title: 'exams', icon: 'icon-receipt-item' },
-  { title: 'reports', icon: 'icon-trend-up', url: '/card' },
+  { title: 'reports', icon: 'icon-trend-up', url: 'card' },
   { title: 'bells', icon: 'icon-menu-board' },
   { title: 'absents', icon: 'icon-personalcard' },
   { title: 'rollCall', icon: 'icon-task' },
